b31f784bbb2bd40dd75ee67d8361942b3feb2e3a
igor-taconi <igortaconi1@gmail.com>
2021-03-13 14:53:25 -0300

    [ADD] Adiciona configurações

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+template_folder = os.path.join(basedir, "resources", "templates")
+static_folder = os.path.join(basedir, "resources", "static")

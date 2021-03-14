d014d4ee432ab13b0225ecd1171d911961225b07
igor-taconi <igortaconi1@gmail.com>
2021-03-14 13:57:31 -0300

    [UPDATE] Atualiza a arquitetura do projeto

---
+++ giselaortt/blueprints/webui/__init__.py
@@ -0,0 +1,14 @@
+from flask import Blueprint
+from .views import index, store, login, blog, about
+
+bp = Blueprint("webui", __name__, template_folder="templates")
+
+bp.add_url_rule("/", view_func=index)
+bp.add_url_rule("/store", view_func=store)
+bp.add_url_rule("/login", view_func=login, endpoint="login")
+bp.add_url_rule("/blog", view_func=blog)
+bp.add_url_rule("/about", view_func=about)
+
+
+def init_app(app):
+    app.register_blueprint(bp)
---
+++ giselaortt/blueprints/webui/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, request, flash
+
+
+def index():
+    name = 'Olárrrrrrrrrrrrrrr'
+    return render_template('index.html', name=name)
+
+
+def store():
+    return render_template('store.html')
+
+
+def login():
+    contact = ''
+
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        contact = f'<Name {name}> {email}'
+        flash('Logado com sucesso!', 'success')
+
+    return render_template('login.html', contact=contact)
+
+
+def blog():
+    return render_template('blog.html')
+
+
+def about():
+    return render_template('about.html')

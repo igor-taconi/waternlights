9c877bc982b8c5dd3b8a7bd764f9028b8cbf3a31
taconi <>
2021-05-22 16:03:40 -0300

    [ADD] hooks; creacao de tabelas do db

--- app/ext/hooks.py
+++ app/ext/hooks.py
@@ -2,14 +2,14 @@ from typing import NoReturn
 
 from flask import Flask
 
-from app.models import Post, User, db
+from app.models import Post, PostCategory,User, db
 
 
 def init_app(app: Flask) -> NoReturn:
     @app.before_first_request
     def _create_tables():
         """Cria as tabelas do banco antes do primeiro request."""
-        MODELS = [User, Post]
+        MODELS = [Post, PostCategory, User]
         db.create_tables(MODELS)
 
     @app.teardown_request

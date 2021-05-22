cd6dafa2b19081a3aaecd9c467d8f7ccf161b073
taconi <>
2021-05-22 16:02:12 -0300

    [UPDATE] models

--- app/models.py
+++ app/models.py
@@ -4,15 +4,15 @@ import peewee as pw
 
 from app.config import settings
 
-# db = pw.SqliteDatabase("database.db")
-db = pw.PostgresqlDatabase(
-    database=settings.get_environ("DB_NAME"),
-    user=settings.get_environ("DB_USER"),
-    password=settings.get_environ("DB_PASSWORD"),
-    host=settings.get_environ("DB_HOST"),
-    port=settings.get_environ("DB_PORT"),
-    autorollback=True,
-)
+db = pw.SqliteDatabase("database.db")
+# db = pw.PostgresqlDatabase(
+#     database=settings.get_environ("DB_NAME"),
+#     user=settings.get_environ("DB_USER"),
+#     password=settings.get_environ("DB_PASSWORD"),
+#     host=settings.get_environ("DB_HOST"),
+#     port=settings.get_environ("DB_PORT"),
+#     autorollback=True,
+# )
 
 
 class BaseModel(pw.Model):
@@ -23,7 +23,11 @@ class BaseModel(pw.Model):
 class User(BaseModel):
     username = pw.CharField(max_length=80)
     email = pw.CharField(max_length=120)
-    pw.TextField()
+    password = pw.TextField()
+
+
+class PostCategory(BaseModel):
+    category = pw.CharField(max_length=30)
 
 
 class Post(BaseModel):
@@ -32,6 +36,7 @@ class Post(BaseModel):
     create_at = pw.DateTimeField(default=datetime.now)
     update_at = pw.DateTimeField(null=True, default=create_at)
     user = pw.ForeignKeyField(User, backref="post", null=True)
+    category = pw.ForeignKeyField(PostCategory, backref="post", null=True)
 
     class Meta:
         order_by = ("-update_at",)
